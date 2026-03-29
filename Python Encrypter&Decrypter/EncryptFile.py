059081c68798cd647c0ebd867662d45d31eda1d7
Display.vr <160807646+Displayvr14@users.noreply.github.com>
2026-03-29 10:17:08 -0700

    Added File Encryption and Decryption

---
+++ Python Encrypter&Decrypter/EncryptFile.py
@@ -0,0 +1,22 @@
+import Encryption
+
+fileDir = input("Enter the name of the file you want to encrypt: ")
+KeyIN = input("Enter Your Key Or Leave Blank For A New Random Key: ")
+if KeyIN == "":
+    Key = Encryption.GenerateKey()
+    print("Generated Key: " + Encryption.GenerateKeyString(Key))
+else:
+    Key = Encryption.KeyStringToList(KeyIN)
+
+fileDir = './' + fileDir
+
+with open(fileDir, 'r') as file:
+    fileData = file.read()
+
+encryptedData = Encryption.Encrypt(fileData, Key)
+
+with open(fileDir, 'w') as file:
+    file.write(encryptedData)
+print("File Encrypted Successfully!")
+input("Press Enter To Exit...")
+# enf*@+Va:BWCQj$~P<>&=3J0?`E%oRtSYu9F6)XkgsTlz#vH/mIb7xcGyD5|!ZU\Ap;'KO,_"i81L(.hMdr- N4q^w2
